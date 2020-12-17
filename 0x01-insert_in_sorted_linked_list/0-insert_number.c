fbc1ce36b9a3b93cf468c3ab43e6d5ec23b24ce1
hamdi458 <hamdi.ghorbel645@gmail.com>
2020-12-17 11:26:34 +0100

    list*

--- 0x01-insert_in_sorted_linked_list/0-insert_number.c
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -1,30 +1,21 @@
-#include "lists.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "lists.h"
+
 /**
  *add_node -  adds a new node at the beginning of a list_t list.
  *@head : head of linked list
  *@str : str of node
  *Return: new node
  */
-list_t *insert_node(listint_t **head, int number)
+listint_t *insert_node(listint_t **head, int number)
 {
-int s = 0;
 listint_t *nouveau = malloc(sizeof(*nouveau));
 if (head == NULL || nouveau == NULL)
 return (NULL);
-if (!number)
-{
-nouveau->str = NULL;
-nouveau->len = 0;
-}
-else
-{
 nouveau->n = number;
-}
 nouveau->next = *head;
 *head = nouveau;
 return (nouveau);
-
 }
