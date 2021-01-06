712a07368faeb4a5b54066dcca9fdf73eb0e6947
hamdi458 <hamdi.ghorbel645@gmail.com>
2021-01-06 04:02:36 +0100

    minimum opp

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -6,7 +6,7 @@ def minOperations(n):
     """Minimum Operations"""
     sum_op = 0
     while n > 1:
-        for i in range(1,round(n / 2)+1):
+        for i in range(1, round(n / 2)+1):
             if n % i == 0:
                 max_div = i
         sum_op += n / max_div
