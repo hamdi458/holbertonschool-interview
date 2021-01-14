520ca94f12bfc77a84292c1f8dc5bfe4454f851b
hamdi458 <hamdi.ghorbel645@gmail.com>
2021-01-14 19:15:33 +0100

    sandpile

--- 0x04-sandpiles/0-main.c
+++ 0x04-sandpiles/0-main.c
@@ -4,81 +4,100 @@
 #include "sandpiles.h"
 
 /**
- * print_grid_sum - Print 3x3 grids sum
- * @grid1: Left 3x3 grid
- * @grid2: Right 3x3 grid
- *
- */
-static void print_grid_sum(int grid1[3][3], int grid2[3][3])
-{
-    int i, j;
-
-    for (i = 0; i < 3; i++)
-    {
-        for (j = 0; j < 3; j++)
-        {
-            if (j)
-                printf(" ");
-            printf("%d", grid1[i][j]);
-        }
-
-        printf(" %c ", (i == 1 ? '+' : ' '));
-
-        for (j = 0; j < 3; j++)
-        {
-            if (j)
-                printf(" ");
-            printf("%d", grid2[i][j]);
-        }
-        printf("\n");
-    }
-}
-
-/**
- * print_grid - Print 3x3 grid
- * @grid: 3x3 grid
+ * print_grid - print grid
+ * @grid: the grid
  *
  */
 static void print_grid(int grid[3][3])
 {
-    int i, j;
+	int i, j;
 
-    for (i = 0; i < 3; i++)
-    {
-        for (j = 0; j < 3; j++)
-        {
-            if (j)
-                printf(" ");
-            printf("%d", grid[i][j]);
-        }
-        printf("\n");
-    }
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			if (j)
+				printf(" ");
+			printf("%d", grid[i][j]);
+		}
+		printf("\n");
+	}
 }
 
 /**
- * main - Entry point
- *
- * Return: EXIT_SUCCESS or EXIT_FAILURE
+ * add - computes the sum of two sandpiles
+ * @grid1: first grid
+ * @grid2: second grid
  */
-int main(void)
+void add(int grid1[3][3], int  grid2[3][3])
 {
-    int grid1[3][3] = {
-        {3, 3, 3},
-        {3, 3, 3},
-        {3, 3, 3}
-    };
-    int grid2[3][3] = {
-        {1, 3, 1},
-        {3, 3, 3},
-        {1, 3, 1}
-    };
+	int i, j;
 
-    print_grid_sum(grid1, grid2);
+	for (i = 0; i < 3; i++)
+		for (j = 0; j < 3; j++)
+			grid1[i][j] += grid2[i][j];
+}
 
-    sandpiles_sum(grid1, grid2);
+/**
+ * maxg - find the max
+ * @grid1: the grid
+ * Return: max of the grid
+ */
+int maxg(int grid1[3][3])
+{
+	int i, j, m = grid1[0][0];
 
-    printf("=\n");
-    print_grid(grid1);
+	for (i = 0; i < 3; i++)
+		for (j = 0; j < 3; j++)
+			if (m < grid1[i][j])
+				m = grid1[i][j];
+	return (m);
+}
 
-    return (EXIT_SUCCESS);
+/**
+ * sandpiles_sum - computes the sum of two sandpiles
+ * @grid1: first grid
+ * @grid2: second grid
+ */
+
+void sandpiles_sum(int grid1[3][3], int grid2[3][3])
+{
+	int i, j, grid[3][3];
+
+	add(grid1, grid2);
+	if (maxg(grid1) > 3)
+	{
+		printf("=\n");
+		print_grid(grid1);
+	}
+	while (maxg(grid1) > 3)
+	{
+		for (i = 0; i < 3; i++)
+			for (j = 0; j < 3; j++)
+				grid[i][j] = 0;
+		for (i = 0; i < 3; i++)
+		{
+			for (j = 0; j < 3; j++)
+			{
+				if (grid1[i][j] > 3)
+				{
+					grid[i][j] -= 4;
+					if (j - 1 < 3 && j - 1 >= 0)
+						grid[i][j - 1] += 1;
+					if (j + 1 < 3 && j + 1 >= 0)
+						grid[i][j + 1] += 1;
+					if (i - 1 < 3 && i - 1 >= 0)
+						grid[i - 1][j] += 1;
+					if (i + 1 < 3 && i + 1 >= 0)
+						grid[i + 1][j] += 1;
+				}
+			}
+		}
+		add(grid1, grid);
+		if (maxg(grid1) > 3)
+		{
+			printf("=\n");
+			print_grid(grid1);
+		}
+	}
 }
