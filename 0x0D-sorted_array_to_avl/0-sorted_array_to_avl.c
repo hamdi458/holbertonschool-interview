f908e0b594584e0246d906b697794b7e5636c137
hamdi458 <hamdi.ghorbel645@gmail.com>
2021-03-25 20:29:14 +0100

    wiw

--- 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
+++ 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
@@ -1,72 +1,72 @@
 #include "binary_trees.h"
 
 /**
-* sorted_array_to_avl - fonction de creation darbre
+* sorted_array_to_avl -function that builds an AVL tree from an array
 * @array: int
-* @size: size_t
-* Return: int(mean true)
+* @size: int
+* Return: pointer to the root node of the created AVL tree, or NULL
 */
 
 avl_t *sorted_array_to_avl(int *array, size_t size)
 {
-	avl_t *arbre;
+avl_t *tree;
 
-	if (array == NULL || size <= 0)
-		return (NULL);
+if (!array || size <= 0)
+return (NULL);
 
-	arbre = ajout_noeud(0, NULL, size - 1, array);
-	return (arbre);
+tree = add_noeud(0, NULL, size - 1, array);
+return (tree);
 }
 
 
 /**
-* ajout_noeud - creation darbre
+* add_noeud - create tree
 * @array: int
-* @debut: size_t
-* @fin: size_t
-* @parent: pointeur
+* @debut: int
+* @fin: int
+* @parent: int
 * Return: pointeur(mean true)
 */
-avl_t *ajout_noeud(size_t debut, avl_t *parent, size_t fin, int *array)
+avl_t *add_noeud(size_t d, avl_t *parent, size_t f, int *array)
 {
-	size_t milieu;
-	avl_t *arbre;
+size_t m;
+avl_t *tree;
 
-	if (debut > fin)
-		return (NULL);
+if (d > f)
+return (NULL);
 
-	milieu = (debut + fin) / 2;
-	arbre = nvx_noeud(parent, array[milieu]);
-	if (arbre == NULL)
-		return (NULL);
+m = (d + f) / 2;
+tree = new_noeud(parent, array[m]);
+if (!tree)
+return (NULL);
 
-	if (milieu != fin)
-		arbre->right = ajout_noeud(milieu + 1, arbre, fin, array);
+if (m != f)
+tree->right = add_noeud(m + 1, tree, f, array);
 
-	if (milieu != debut)
-		arbre->left = ajout_noeud(debut, arbre, milieu - 1, array);
+if (m != d)
+tree->left = add_noeud(d, tree, m - 1, array);
 
-	return (arbre);
+return (tree);
 }
 
 /**
-* nvx_noeud - creation de nouveau noeud
+* new_noeud - creation de nouveau noeud
 * @parent: pointeur
 * @n: int
 * Return: noeud(mean true)
 */
-avl_t *nvx_noeud(avl_t *parent, size_t n)
+avl_t *new_noeud(avl_t *p, size_t n)
 {
-	avl_t *nouveau;
+avl_t *new;
 
-	nouveau = malloc(sizeof(avl_t));
+new = malloc(sizeof(avl_t));
 
-	if (nouveau == NULL)
-		return (NULL);
+if (!new)
+return (NULL);
 
-	nouveau->n = n;
-	nouveau->right = NULL;
-	nouveau->parent = parent;
-	nouveau->left = NULL;
-	return (nouveau);
+new->n = n;
+new->right = NULL;
+new->parent = p;
+new->left = NULL;
+return (new);
 }
