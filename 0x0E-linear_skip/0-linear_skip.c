c2b057440f5a4ef4752c74c1b5218a929fd1dca0
hamdi458 <hamdi.ghorbel645@gmail.com>
2021-04-01 22:27:26 +0100

    wiw

--- 0x0E-linear_skip/0-linear_skip.c
+++ 0x0E-linear_skip/0-linear_skip.c
@@ -1,53 +1,60 @@
 #include "search.h"
 /**
- * linear_skip - fn
- * @list: list
+ * find - fn
+ * @p: p
+ * @actu: actu
  * @value: val
- * Return: NULL
+ * Return: node or null
+ */
+skiplist_t *find(skiplist_t *p, skiplist_t *actu, int value)
+{
+while (p && p->index <= actu->index)
+{
+printf("Value checked at index [%lu] = [%i]\n", p->index, p->n);
+if (p->n == value)
+{
+return (p);
+}
+p = p->next;
+}
+return (NULL);
+}
+
+/**
+ * linear_skip - function that searches for a value
+ * @list: list of search
+ * @value: value
+ * Return: the pointer or NULL
  */
 skiplist_t *linear_skip(skiplist_t *list, int value)
 {
-if (list == NULL)
+skiplist_t *actu, *p;
+size_t actut, pv;
+if (!list)
 return (NULL);
-skiplist_t *sctr = list, *tmpsctr = NULL;
-size_t tempIndex = 0;
-char *verif = "Value checked at index [%lu] = [%d]\n";
-char *f = "Value f between indexes [%lu] and [%lu]\n";
-
-while (sctr->express)
+actu = list;
+while (actu->express != NULL)
 {
-if (sctr->express->n == value)
-{
-printf("Value checked at index [%lu] = [%d]\n", sctr->index, sctr->n);
-return (sctr->express);
-}
-if (sctr->express->n > value)
-{
-printf(verif, sctr->express->index, sctr->express->n);
-printf(f, sctr->index, sctr->express->index);
-while (sctr->next)
-{
-printf("Value checked at index [%lu] = [%d]\n", sctr->index, sctr->n);
-if (sctr->n == value)
-return (sctr);
-sctr = sctr->next;
-}
-}
-sctr = sctr->express;
-printf("Value checked at index [%lu] = [%d]\n", sctr->index, sctr->n);
-}
-tmpsctr = sctr;
-tempIndex = sctr->index;
-while (sctr->next)
-{
-sctr = sctr->next;
-}
-printf(f, tempIndex, sctr->index);
-while (tmpsctr->next)
-{
-printf(verif, tmpsctr->index, tmpsctr->n);
-tmpsctr = tmpsctr->next;
-}
-printf(verif, tmpsctr->index, tmpsctr->n);
+p = actu;
+actu = actu->express;
+actut = actu->index;
+pv = p->index;
+printf("Value checked at index [%lu] = [%i]\n", actut, actu->n);
+if (p->n > value && p->index == 0)
 return (NULL);
+if (actu->n >= value)
+{
+break;
+}
+}
+if (!actu->express && actu->n < value)
+{
+p = actu;
+pv = p->index;
+while (actu->next)
+actu = actu->next;
+actut = actu->index;
+}
+printf("Value found between indexes [%lu] and [%lu]\n", pv, actut);
+return (find(p, actu, value));
 }
