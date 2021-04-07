1eb3ebe21451ea94933dfa51a275efecfe121871
hamdi458 <hamdi.ghorbel645@gmail.com>
2021-04-07 16:49:17 +0100

    fix semistandard

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,13 +1,12 @@
 #!/usr/bin/node
 const request = require('request');
-const filmId = process.argv[2];
-const url = 'https://swapi-api.hbtn.io/api/films/'+process.argv[2]+'/';
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2] + '/';
 request(url, async (error, response, body) => {
   if (!error) {
     const characters = JSON.parse(body).characters;
-    for (const id_char in characters) {
+    for (const char in characters) {
       await new Promise((resolve, reject) => {
-        request(characters[id_char], (error, response, body) => {
+        request(characters[char], (error, response, body) => {
           if (!error) {
             console.log(JSON.parse(body).name);
             resolve();
