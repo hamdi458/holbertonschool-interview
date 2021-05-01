f63ffb0ad3ce20ef6113dd467776b70ed9b30e88
hamdi458 <hamdi.ghorbel645@gmail.com>
2021-05-01 13:44:05 +0100

    first

---
+++ 0x0B-menger/0-menger.c
@@ -0,0 +1,33 @@
+#include "menger.h"
+
+/**
+ * menger - Fn that draws a 2D Menger Sponge
+ * @level: level
+ * Return: 0
+*/
+void menger(int level)
+{
+	int i, j, size, h, w;
+	char sim;
+
+	size = pow(3, level);
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			h = i;
+			w = j;
+			sim = '#';
+			while (h > 0)
+			{
+				if (h % 3 == 1 && w % 3 == 1)
+					sim = ' ';
+				h = h / 3;
+				w = w / 3;
+			}
+			printf("%c", sim);
+		}
+		printf("\n");
+	}
+}
---
+++ 0x0B-menger/menger.h
@@ -0,0 +1,11 @@
+ 
+#ifndef MENGER_H_
+#define MENGER_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+void menger(int level);
+
+#endif /* MENGER_H_ */
