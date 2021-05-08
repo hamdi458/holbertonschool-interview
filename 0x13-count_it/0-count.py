1db2901099f20b2211c70041638c372ff68fad31
hamdi458 <hamdi.ghorbel645@gmail.com>
2021-05-08 23:13:36 +0100

    fix pycodestyle

--- 0x13-count_it/0-count.py
+++ 0x13-count_it/0-count.py
@@ -9,7 +9,8 @@ def count_words(subreddit, list, after="", count={}):
         return []
     url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)\
-                AppleWebKit/537.36 (KHTML, like Gecko)Chrome/70.0.3538.77 Safari/537.36'}
+                AppleWebKit/537.36 (KHTML, like Gecko)Chrome/70.0.3538.7\
+                7 Safari/537.36'}
     if after:
         url += '?after={}'.format(after)
     r = requests.get(url, headers=headers, allow_redirects=False)
