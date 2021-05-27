b3a25789b680048ed91d457315311a9ce4dbb5d8
hamdi458 <hamdi.ghorbel645@gmail.com>
2021-05-27 17:36:25 +0100

    first

--- 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -5,7 +5,7 @@
 def rotate_2d_matrix(matrix):
     """Given an n x n 2D matrix, rotate it 90 degrees clockwise"""
     le = len(matrix[0]) - 1
-    for i in range(le // 2):
+    for i in range(le + 1 // 2):
         for j in range(i, le - i):
             temp = matrix[i][j]
             matrix[i][j] = matrix[le - j][i]
